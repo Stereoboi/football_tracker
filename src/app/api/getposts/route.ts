7d68970ced5e59d5f68028bff694e19b1de7cd16
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-04 00:28:23 +0300

    dashboard

---
+++ src/app/api/getposts/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse, NextRequest } from "next/server";
+import connect from "../../../../util/mongoConnect";
+import Post from "@/models/Post";
+
+export const GET = async (request: Request) => {
+  const url = new URL(request.url);
+
+  let step = url.searchParams.get("step");
+  let lim = 6;
+  if (!step) {
+    step = "1";
+    lim = 0;
+  }
+  const skip = (Number(step) - 1) * 6;
+
+  try {
+    await connect();
+
+    const posts = await Post.find().limit(lim).skip(skip);
+
+    return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
