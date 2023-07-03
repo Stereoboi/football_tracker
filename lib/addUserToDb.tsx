fff3ad73b9da74d9e9ab222862cd8131f7534e71
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-03 19:02:26 +0300

    fix

--- lib/addUserToDb.tsx
+++ lib/addUserToDb.tsx
@@ -1,12 +1,15 @@
 export default async function addUserToDatabase(name: any, email: any) {
-  const res = await fetch("http://localhost:3000/api/addusertodb", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name,
-      email,
-    }),
-  });
+  const res = await fetch(
+    "https://football-tracker-steel.vercel.app/api/addusertodb",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+      }),
+    }
+  );
 }
