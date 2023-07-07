01e08082998cec0003ef24e69c63552a714796f0
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-07 14:18:23 +0300

    SSG_fix

--- lib/getAllNewsFromDb.ts
+++ lib/getAllNewsFromDb.ts
@@ -1,7 +1,10 @@
 export default async function getAllNewsFromDb() {
-  const res = await fetch(`http://localhost:3000/api/getallposts`, {
-    method: "GET",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_DEPLOY_URL}/api/getallposts`,
+    {
+      method: "GET",
+    }
+  );
 
   if (!res.ok) return undefined;
 
