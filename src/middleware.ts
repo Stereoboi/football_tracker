a10dd5dd9f56e7c25525074fefd4248518392b10
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-07 20:29:57 +0300

    main

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,15 +32,15 @@
 //   ],
 // };
 
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// // This function can be marked `async` if using `await` inside
-// export function middleware(request: NextRequest) {
-//   return NextResponse.redirect(new URL("/standings", request.url));
-// }
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL("/standings", request.url));
+}
 
-// // See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: "/check",
-// };
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/check",
+};
