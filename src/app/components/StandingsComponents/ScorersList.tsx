1fde1ab12981c8ec8c01bb19635247da43b426ef
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-08 19:53:53 +0300

    fix_scorrer_list

--- src/app/components/StandingsComponents/ScorersList.tsx
+++ src/app/components/StandingsComponents/ScorersList.tsx
@@ -12,62 +12,56 @@ export default function ScorersList({
   console.log(error);
 
   return (
-    <>
-      {error === process.env.NEXT_PUBLIC_ERROR ? (
-        <Error error={error} />
-      ) : (
-        result.map((el, index) => (
-          <div
-            key={el.player.id}
-            className="relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
-          >
-            <div className="relative bg-gradient-to-br from-pink-500 to-orange-400 rounded-lg">
-              <div className="flex flex-col items-center w-auto h-auto justify-center p-2">
-                <img
-                  src={el.player.photo}
-                  alt={el.player.name}
-                  className="rounded-full shadow-md"
-                />
+    <div className="relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      {result.map((el, index) => (
+        <div
+          key={el.player.id}
+          className="relative bg-gradient-to-br from-pink-500 to-orange-400 rounded-lg"
+        >
+          <div className="flex flex-col items-center w-auto h-auto justify-center p-2">
+            <img
+              src={el.player.photo}
+              alt={el.player.name}
+              className="rounded-full shadow-md"
+            />
 
-                <p className="absolute top-2 left-2 rounded-full  flex justify-center items-center z-10 text-black">
-                  #{index + 1}
+            <p className="absolute top-2 left-2 rounded-full  flex justify-center items-center z-10 text-black">
+              #{index + 1}
+            </p>
+          </div>
+          <div className="p-2">
+            <div className=" rounded-lg">
+              <p className="text-gray-900 text-center">{el.player.name}</p>
+              <div className="flex flex-col items-center">
+                <p className="text-gray-900 text-center pt-2">
+                  {el.statistics[0].team.name}
                 </p>
+                <img
+                  src={el.statistics[0].team.logo}
+                  alt={el.statistics[0].team.name}
+                  width={40}
+                  height={40}
+                  className=""
+                />
               </div>
-              <div className="p-2">
-                <div className=" rounded-lg">
-                  <p className="text-gray-900 text-center">{el.player.name}</p>
-                  <div className="flex flex-col items-center">
-                    <p className="text-gray-900 text-center pt-2">
-                      {el.statistics[0].team.name}
-                    </p>
-                    <img
-                      src={el.statistics[0].team.logo}
-                      alt={el.statistics[0].team.name}
-                      width={40}
-                      height={40}
-                      className=""
-                    />
-                  </div>
-                  <div className="text-center">
-                    <p className="text-gray-900 pt-2">
-                      Games: {el.statistics[0].games.appearences}
-                    </p>
-                    <div className="flex justify-center">
-                      <p className="text-gray-900 pt-2 mr-1">
-                        Goals: {el.statistics[0].goals.total}
-                      </p>
-                      <p className="text-gray-900 pt-2 ml-1">
-                        Assists: {el.statistics[0].goals.assists}
-                      </p>
-                    </div>
-                  </div>
+              <div className="text-center">
+                <p className="text-gray-900 pt-2">
+                  Games: {el.statistics[0].games.appearences}
+                </p>
+                <div className="flex justify-center">
+                  <p className="text-gray-900 pt-2 mr-1">
+                    Goals: {el.statistics[0].goals.total}
+                  </p>
+                  <p className="text-gray-900 pt-2 ml-1">
+                    Assists: {el.statistics[0].goals.assists}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        ))
-      )}
-    </>
+        </div>
+      ))}
+    </div>
   );
 }
 //  <div className="relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
--- src/app/standings/[standingsId]/topscorers/page.tsx
+++ src/app/standings/[standingsId]/topscorers/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ScorersList from "@/app/components/StandingsComponents/ScorersList";
 import getTopScorers from "../../../../../lib/getTopScorrers";
 import { ScorersApiResponse } from "../../../../../types/topScorers";
+import Error from "../../../components/Error/Error";
 type Params = {
   params: {
     standingsId: string;
@@ -28,11 +29,19 @@ export async function generateMetadata({ params: { standingsId } }: Params) {
 
 export default async function TopScorers({ params: { standingsId } }: Params) {
   const result: ScorersApiResponse = await getTopScorers(standingsId);
-  console.log(result);
+
+  const error = result.errors.requests;
 
   return (
-    <div>
-      <ScorersList scorers={result} />
-    </div>
+    <>
+      {error === process.env.NEXT_PUBLIC_ERROR ? (
+        <Error error={error} />
+      ) : (
+        <ScorersList scorers={result} />
+      )}
+    </>
   );
 }
+// <div>
+//   <ScorersList scorers={result} />
+// </div>
