2ce44fa09837b887f7f4772b03e46058943dfecb
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-09 15:06:25 +0300

    rev_chang

--- src/app/api/leaguesId/route.ts
+++ src/app/api/leaguesId/route.ts
@@ -15,10 +15,13 @@ export async function GET() {
   const data = await res.json();
 
   const leaguesIds = [39, 78, 135, 140, 61, 88];
+  console.log(leaguesIds);
 
   const filteredData = data.response.filter((el: ResponseItem) =>
     leaguesIds.includes(el.league.id)
   );
+
+  console.log(filteredData);
 
   if (!res.ok) return undefined;
 
