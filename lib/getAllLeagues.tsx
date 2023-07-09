87bae3b4393e9d3063d97647f7b5813b9de48525
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-09 16:04:30 +0300

    main

--- lib/getAllLeagues.tsx
+++ lib/getAllLeagues.tsx
@@ -1,11 +1,9 @@
 // process.env.NEXT_FOOTBALL_API_KEY;
 
-// export default async function getAllLeagues() {
-//   const res = await fetch(
-//     `${process.env.NEXT_PUBLIC_DEPLOY_URL}/api/leaguesId`
-//   );
-
-//   if (!res.ok) return undefined;
-
-//   return res.json();
-// }
+export default async function getAllLeagues() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_DEPLOY_URL}/api/leaguesId`
+  );
+  if (!res.ok) return undefined;
+  return res.json();
+}
